464f35ce947bea651b43fe39c3ccbae9223af6e5
MKdivine <jensboerger23@gmail.com>
2025-07-14 19:19:08 +0200

    lol

--- app.js
+++ app.js
@@ -22,20 +22,16 @@ function infixToPostfix(tokens) {
     for (let token of tokens) {
         if (!isNaN(token)) {
             outputQueue.push(token); // Wenn Zahl, direkt in die Output-Queue
-        } else if ("+-*/^".includes(token)) {
-            // Wenn Operator:
-            // Solange der letzte Operator im Stack gleich oder höher ist,
-            // -> den Stack-Operator in die Output-Queue verschieben
-            // Dann den neuen Operator auf den Stack legen
-            if (getPrecedence(token) === 1) {
-             
-            }
-            
-            else if (getPrecedence(token) === 2) {
-            }
-            else if (getPrecedence(token) === 3) {
-            }
-        }
+       } else if ("+-*/^".includes(token)) {
+  while (
+    operatorStack.length > 0 &&
+    getPrecedence(operatorStack.at(-1)) >= getPrecedence(token)
+  ) {
+    outputQueue.push(operatorStack.pop());
+  }
+  operatorStack.push(token);
+}
+
     }
 
     // Alle restlichen Operatoren vom Stack in die Output-Queue
